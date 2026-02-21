818a5f4535d6d7a829ca3080329237dd9fe13962
Ryan Azimi <replywithyes@gmail.com>
2026-02-21 18:01:22 -0500

    Added boxing ring collider

--- boxing-jack/Scripts/Main/main.gd
+++ boxing-jack/Scripts/Main/main.gd
@@ -1,9 +1,10 @@
 extends Node
-
+var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = preload("res://Scenes/Phys_Player.tscn").instantiate()
+	add_child(player)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
